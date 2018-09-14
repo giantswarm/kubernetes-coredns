d84b2336740516487550e3f493e3105c0040e0a7
Ross Fairbanks <rossf7@users.noreply.github.com>
2018-09-14 09:46:11 +0200

    vendor: updated e2esetup and test wiring (#10)
    * vendor: updated e2esetup
    
    * Update test wiring

--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -65,9 +65,10 @@ func init() {
 
 	{
 		c := framework.HostConfig{
-			Logger:     l,
-			ClusterID:  "na",
-			VaultToken: "na",
+			Logger: l,
+
+			ClusterID:  "n/a",
+			VaultToken: "n/a",
 		}
 		h, err = framework.NewHost(c)
 		if err != nil {
@@ -88,9 +89,10 @@ func init() {
 
 	{
 		c := helmclient.Config{
-			Logger:          l,
-			K8sClient:       h.K8sClient(),
-			RestConfig:      h.RestConfig(),
+			Logger:     l,
+			K8sClient:  h.K8sClient(),
+			RestConfig: h.RestConfig(),
+
 			TillerNamespace: "giantswarm",
 		}
 		helmClient, err = helmclient.New(c)
@@ -149,10 +151,11 @@ func TestMain(m *testing.M) {
 			Host:       h,
 		}
 
-		err := e2esetup.Setup(ctx, m, c)
+		v, err := e2esetup.Setup(ctx, m, c)
 		if err != nil {
 			l.LogCtx(ctx, "level", "error", "message", "e2e test failed", "stack", fmt.Sprintf("%#v\n", err))
-			os.Exit(1)
 		}
+
+		os.Exit(v)
 	}
 }
